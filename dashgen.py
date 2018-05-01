d6f83444443480fe2c88d043e7530f16e7575655
amritvin <c.amritanand@gmail.com>
2018-05-01 16:30:42 +0530

    with progress bar

--- dashgen.py
+++ dashgen.py
@@ -5,6 +5,17 @@ import json
 import sys
 import pprint
 import timeit
+import time
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
+
 
 start = timeit.default_timer()
 for _, dirnames, filenames in os.walk('.'):
@@ -13,11 +24,14 @@ arr=filenames
 cmd = ["md5sum ","sha1sum ","sha256sum ","sha512sum "] 
 result={}
 count=0
+tot=len(arr)
 for files in arr:
 	hash={}
  	if files[0]==".":
+ 		tot=tot-1
  		continue
  	count=count+1
+ 	progress(count+1,tot,"processing")
 	for c in cmd:
 		cc=c+files
 		proc = subprocess.Popen(cc.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -28,7 +42,7 @@ for files in arr:
 	hash["size"]=os.stat(files).st_size 
 	result[files]=hash
 stop = timeit.default_timer()
+os.system('clear')
 #print json.dumps(result)
 pprint.pprint(result)
 print "\n\t\t\tDone!  ?(`_`)?  Hashes of "+str(count)+" files calculated in "+ str(stop-start)+" secs\n"
-
